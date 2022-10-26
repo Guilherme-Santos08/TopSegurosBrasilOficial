62e553a9da74bb1d60eafb513e9e34e47fdafa6c
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-10-26 10:47:03 -0300

    feat: layout apolice

--- TopSegurosBrasilTest/Models/Apolice.cs
+++ TopSegurosBrasilTest/Models/Apolice.cs
@@ -8,13 +8,25 @@ namespace TopSegurosBrasil.Models
         [Key()]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Inicio do Contrato")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataInicioContrato { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Vencimento do Contrato")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataVencimento { get; set; }
 
         [ForeignKey("Veiculo")]
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Veiculo Id")]
         public int VeiculoId { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Seguro Escolhido")]
         public PlanoDoSeguro PlanoDoSeguro { get; set; }
         public int PlanoDoSeguroId { get; set; }
 
