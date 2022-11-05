45aae539d2a1d86d13527f4f7a0f2c38b1c731ad
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-11-05 20:21:40 -0300

    feat: novo layout do sinistro

--- TopSegurosBrasilTest/Models/Sinistro.cs
+++ TopSegurosBrasilTest/Models/Sinistro.cs
@@ -7,9 +7,21 @@ namespace TopSegurosBrasil.Models
     {
         [Key()]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Data do Sinistro")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string DataSinistro { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Descrição do Sinistro")]
         public string DescSinistro { get; set; }
+
+
         [ForeignKey("Veiculo")]
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Id do Veiculo")]
         public int VeiculoId { get; set; }
 
         public Sinistro() { }
