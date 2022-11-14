65353179ec3cd31d7cce0a3bd62f13910cee5249
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-11-14 11:29:43 -0300

    refactor: novo sidebar

--- TopSegurosBrasilTest/tailwind.config.js
+++ TopSegurosBrasilTest/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.cshtml", "**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["**/*.cshtml", "**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'science-blue': {
+                    DEFAULT: '#003ED2',
+                    50: '#8BADFF',
+                    100: '#769FFF',
+                    200: '#4D82FF',
+                    300: '#2565FF',
+                    400: '#004AFB',
+                    500: '#003ED2',
+                    600: '#002D9A',
+                    700: '#001D62',
+                    800: '#000C2A',
+                    900: '#000000',
+                },
+            },
+        },
+    },
+    plugins: [],
 }
