fe283a7e7a02047a8b5764aae79336dc4a0f3e65
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-12-05 20:05:02 -0300

    fix: limitando inputs dos prestadores

--- TopSegurosBrasilTest/Models/Prestador.cs
+++ TopSegurosBrasilTest/Models/Prestador.cs
@@ -6,16 +6,45 @@ namespace TopSegurosBrasil.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Nome da Empresa")]
         public string NomeEmpresa { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "CNPJ")]
         public string Cnpj { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Tipo do Prestador")]
         public string TipoPrestador { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [EmailAddress(ErrorMessage = "Coloque um email válido")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Telefone")]
         public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Bairro")]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Cidade")]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Estado")]
         public string Estado { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "CEP")]
         public int Cep { get; set; }
+
+        [Required(ErrorMessage = "{0} precisa ser preenchido")]
+        [Display(Name = "Número do Imóvel")]
         public int Num_Imovel { get; set; }
 
         public Prestador() { }
